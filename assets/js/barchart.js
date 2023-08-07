01b153c8c22a8570311afe727390fb8fb232b881
Paul Goldschmidt <github@goldschmidt.id>
2023-08-07 15:43:35 +0200

    wait for js load first

--- assets/js/barchart.js
+++ assets/js/barchart.js
@@ -1,29 +1,31 @@
-(function($) {
-    function generateBarGraph(wrapper) {
-      // Set Up Values Array
-      var values = [];
-  
-      // Get Values and save to Array
-      $(wrapper + ' .bar').each(function(index, el) {
-        values.push($(this).data('value'));
-      });
-  
-      // Set max value to 10 (points)
-      var max_value = 10;
-  
-      // Set width of bar to percent of max value
-      $(wrapper + ' .bar').each(function(index, el) {
-        var bar = $(this),
-            value = bar.data('value'),
-            percent = Math.ceil((value / max_value) * 100);
-        // Set Width & Add Class
-        bar.width(percent + '%');
-        bar.addClass('in');
-      });
-    }
-  
-    // Generate the bar graph on window load...
-    $(window).on('load', function(event) {
-      generateBarGraph('#dashboard-stats');
-    });
-  })(jQuery); // Fully reference jQuery after this point.
+$(document).ready(function() {
+    (function($) {
+        function generateBarGraph(wrapper) {
+        // Set Up Values Array
+        var values = [];
+    
+        // Get Values and save to Array
+        $(wrapper + ' .bar').each(function(index, el) {
+            values.push($(this).data('value'));
+        });
+    
+        // Set max value to 10 (points)
+        var max_value = 10;
+    
+        // Set width of bar to percent of max value
+        $(wrapper + ' .bar').each(function(index, el) {
+            var bar = $(this),
+                value = bar.data('value'),
+                percent = Math.ceil((value / max_value) * 100);
+            // Set Width & Add Class
+            bar.width(percent + '%');
+            bar.addClass('in');
+        });
+        }
+    
+        // Generate the bar graph on window load...
+        $(window).on('load', function(event) {
+        generateBarGraph('#dashboard-stats');
+        });
+    })(jQuery); // Fully reference jQuery after this point.
+});
